68e79241f782e9dd880339f9e7df3afd83fed52a
Gabriel Oliveira <gabriel.silva3594@etec.sp.gov.br>
2024-09-13 17:10:38 -0300

    My First Spa

---
+++ my-first-spa/src/components/default/ContentPage.vue
@@ -0,0 +1,39 @@
+<template>
+    <section>
+        <div>
+            <p>Meu Conteúdo Raso e Fútil</p>
+            <div>
+                <input v-model="nome" placeholder="Digite seu Nome">
+                <p>Seu Nome é: {{ nome }}</p>
+                <button @click="submeterFormulário()">Verificar Nome</button>
+
+                <p v-if="url">
+                    {{  url  }}
+                </p>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+
+export default {
+    name:'ContentPage',
+    data() {
+        return{
+            nome: 'Filho do Bill Amostradinho Só Fé da Silva'
+        }
+    },
+    methods: {
+        submeterFormulário() {
+            let response =
+            fetch.get('https://api.github.com/users/${this.nome}').then(() => {
+                this.url = data.url;
+                console.log(data.url);
+            });
+
+            console.log(response);
+        }
+    }
+}
+</script>
